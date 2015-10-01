b7886b5fc1f3023d14bc613a298b344268a41760
taapesh <starmansix@gmail.com>
2015-10-01 18:02:09 -0500

    Added basic activities

---
+++ app/src/main/java/com/example/taapesh/androidrestaurant/StartActivity.java
@@ -0,0 +1,115 @@
+package com.example.taapesh.androidrestaurant;
+
+import android.graphics.Point;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.content.Intent;
+import android.widget.RelativeLayout;
+
+public class StartActivity extends Activity {
+    // Screen and tab bar dimensions
+    private static int screenWidth;
+    private static int screenHeight;
+    private static float screenDensity;
+
+    // Action buttons
+    protected Button goToLoginButton;
+    protected Button goToRegistrationButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+        screenDensity = getResources().getDisplayMetrics().density;
+
+        goToLoginButton = (Button) findViewById(R.id.goToLoginButton);
+        goToRegistrationButton = (Button) findViewById(R.id.goToRegistrationButton);
+
+        // Create login button listener
+        goToLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go to login page
+                Intent goToLogin = new Intent(StartActivity.this, UserLoginActivity.class);
+                startActivity(goToLogin);
+            }
+        });
+
+        // Create register button listener
+        goToRegistrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go to registration page
+                Intent goToRegistration = new Intent(StartActivity.this, UserRegistrationActivity.class);
+                startActivity(goToRegistration);
+            }
+        });
+
+        setupUI();
+    }
+
+    /**
+     * Get all screen dimensions and setup tab bar dimensions
+     */
+    private void setupUI() {
+        // Get screen dimensions
+        WindowManager w = getWindowManager();
+        Point size = new Point();
+        w.getDefaultDisplay().getSize(size);
+        screenWidth = size.x;
+        screenHeight = size.y;
+
+        int btnWidth = (int) (screenWidth / 2 - dpToPx(1)/2.0f);
+        int btnHeight = dpToPx(68);
+        Button loginBtn = (Button) findViewById(R.id.goToLoginButton);
+        Button registerBtn = (Button) findViewById(R.id.goToRegistrationButton);
+
+        // Set login and registration button specs
+        RelativeLayout.LayoutParams rParams = new RelativeLayout.LayoutParams(
+                btnWidth, btnHeight);
+        rParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        rParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
+        loginBtn.setLayoutParams(rParams);
+
+        rParams = new RelativeLayout.LayoutParams(
+                btnWidth, btnHeight);
+        rParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
+        rParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+        registerBtn.setLayoutParams(rParams);
+    }
+
+    /**
+     * Convert dp to pixels
+     */
+    private int dpToPx(int dp) {
+        return Math.round((float)dp * screenDensity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_launcher, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/taapesh/androidrestaurant/UserRegistrationActivity.java
+++ app/src/main/java/com/example/taapesh/androidrestaurant/UserRegistrationActivity.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class HomeActivity extends AppCompatActivity {
+public class UserRegistrationActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_user_registration);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+        getMenuInflater().inflate(R.menu.menu_user_registration, menu);
         return true;
     }
 
