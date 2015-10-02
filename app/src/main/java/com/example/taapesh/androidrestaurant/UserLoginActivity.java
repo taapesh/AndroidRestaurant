14b4fb1de6d96e5b350fe1a6dc58d4b7d299f971
taapesh <starmansix@gmail.com>
2015-10-01 19:18:54 -0500

    Added back navigation on action bar

--- app/src/main/java/com/example/taapesh/androidrestaurant/UserLoginActivity.java
+++ app/src/main/java/com/example/taapesh/androidrestaurant/UserLoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.taapesh.androidrestaurant;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,6 +12,9 @@ public class UserLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_login);
+
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     @Override
@@ -25,11 +29,11 @@ public class UserLoginActivity extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
